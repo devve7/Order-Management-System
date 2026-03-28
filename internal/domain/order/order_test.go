bce5d1d04e8ebf2b162d49f0898febf3acb6574c
Nikoltsev Vladimir <466892@niuitmo.ru>
2026-03-28 11:46:58 +0300

    Add TestAddItem

--- internal/domain/order/order_test.go
+++ internal/domain/order/order_test.go
@@ -5,6 +5,78 @@ import (
 	"testing"
 )
 
+func TestAddItem(t *testing.T) {
+	orderID, _ := NewOrderID("orderID")
+	CustomerID, _ := NewCustomerID("customerID")
+	tests := []struct {
+		name        string
+		item        *OrderItem
+		setup       func() *Order
+		expectedErr error
+	}{
+		{
+			name: "ok",
+			item: &OrderItem{
+				id:    1,
+				name:  "hello",
+				price: 100,
+			},
+			setup: func() *Order {
+				order := NewOrder(orderID, CustomerID)
+				return order
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "add nil item",
+			item: nil,
+			setup: func() *Order {
+				order := NewOrder(orderID, CustomerID)
+				return order
+			},
+			expectedErr: ErrOrderItemNotFound,
+		},
+		{
+			name: "add for cancelled",
+			item: &OrderItem{
+				id:    1,
+				name:  "hello",
+				price: 100,
+			},
+			setup: func() *Order {
+				order := NewOrder(orderID, CustomerID)
+				order.status = StatusCancelled
+				return order
+			},
+			expectedErr: ErrOrderCancelled,
+		},
+		{
+			name: "add for shipped",
+			item: &OrderItem{
+				id:    1,
+				name:  "hello",
+				price: 100,
+			},
+			setup: func() *Order {
+				order := NewOrder(orderID, CustomerID)
+				order.status = StatusShipped
+				return order
+			},
+			expectedErr: ErrOrderShipped,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			order := tt.setup()
+			err := order.AddItem(tt.item)
+			if !errors.Is(err, tt.expectedErr) {
+				t.Errorf("expected (%v), got (%v)", tt.expectedErr, err)
+			}
+		})
+	}
+}
+
 func TestRemoveItem(t *testing.T) {
 	orderID, _ := NewOrderID("orderID")
 	CustomerID, _ := NewCustomerID("customerID")
@@ -54,7 +126,7 @@ func TestRemoveItem(t *testing.T) {
 			name: "delete from cancelled",
 			setup: func() *Order {
 				order := NewOrder(orderID, CustomerID)
-				order.Cancel()
+				order.status = StatusCancelled
 				return order
 			},
 			expectedErr: ErrOrderCancelled,
@@ -63,9 +135,7 @@ func TestRemoveItem(t *testing.T) {
 			name: "delete from shipped",
 			setup: func() *Order {
 				order := NewOrder(orderID, CustomerID)
-				order.AddItem(item)
-				order.Pay()
-				order.Ship()
+				order.status = StatusShipped
 				return order
 			},
 			expectedErr: ErrOrderShipped,
