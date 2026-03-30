7a98e9ee4c991c60e63680196cb8554b306e9d03
Nikoltsev Vladimir <466892@niuitmo.ru>
2026-03-30 16:17:49 +0300

    update catalog

--- internal/infractructure/catalog/inmemory/order_catalog.go
+++ internal/infractructure/catalog/inmemory/order_catalog.go
@@ -1,10 +1,14 @@
 // Package inmemory ...
 package inmemory
 
-import do "Order-Management-System/internal/domain/order"
+import (
+	do "Order-Management-System/internal/domain/order"
+	"sync"
+)
 
 type InMemoryOrderCatalog struct {
 	data map[string]do.Product
+	mtx  sync.RWMutex
 }
 
 func NewInMemoryOrderCatalog() *InMemoryOrderCatalog {
@@ -14,13 +18,17 @@ func NewInMemoryOrderCatalog() *InMemoryOrderCatalog {
 }
 
 func (c *InMemoryOrderCatalog) GetProduct(name string) (do.Product, error) {
+	c.mtx.RLock()
+	defer c.mtx.RUnlock()
 	product, ok := c.data[name]
 	if !ok {
-		return do.Product{}, do.ErrUnknownOrderItem
+		return do.Product{}, do.ErrProductNotFound
 	}
 	return product, nil
 }
 
 func (c *InMemoryOrderCatalog) AddProduct(product do.Product) {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
 	c.data[product.Name] = product
 }
