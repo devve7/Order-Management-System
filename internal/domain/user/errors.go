a5f31eb39b6f8139d5249fdbc9f189b3eee64999
Nikoltsev Vladimir <466892@niuitmo.ru>
2026-06-10 15:10:58 +0300

    add repository interface

--- internal/domain/user/errors.go
+++ internal/domain/user/errors.go
@@ -7,4 +7,5 @@ var (
 	ErrInvalidEmail      = errors.New("invalid email")
 	ErrInvalidRole       = errors.New("invalid role")
 	ErrEmptyPasswordHash = errors.New("empty password hash")
+	ErrUserNotFound      = errors.New("user not found")
 )
